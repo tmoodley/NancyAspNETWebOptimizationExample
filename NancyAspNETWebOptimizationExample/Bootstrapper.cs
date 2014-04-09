ba9778cf4eca68a7b0fb11c946a4a54143cd44bc
stanislavromanov <stanislav.romanov@outlook.com>
2014-04-09 13:50:52 +0300

    add example project

---
+++ NancyAspNETWebOptimizationExample/Bootstrapper.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+using Nancy;
+using Nancy.Bootstrapper;
+using Nancy.Conventions;
+using Nancy.TinyIoc;
+
+namespace NancyAspNETWebOptimizationExample
+{
+    public class Bootstrapper : DefaultNancyBootstrapper
+    {
+        protected override void ConfigureConventions(NancyConventions nancyConventions)
+        {
+            nancyConventions.StaticContentsConventions
+                .Add(StaticContentConventionBuilder.AddDirectory("/js"));
+
+            nancyConventions.StaticContentsConventions
+                .Add(StaticContentConventionBuilder.AddDirectory("/css"));
+        }
+
+        protected override void ApplicationStartup(TinyIoCContainer container, IPipelines pipelines)
+        {
+            var scriptBundle = new ScriptBundle("~/js")
+                .IncludeDirectory("~/Content/js/", "*.js");
+
+            var styleBundle = new StyleBundle("~/css")
+                .IncludeDirectory("~/Content/css", "*.css");
+
+            BundleTable.Bundles.Add(scriptBundle);
+            BundleTable.Bundles.Add(styleBundle);
+
+            BundleTable.EnableOptimizations = true;
+        }
+    }
+}
---
+++ NancyAspNETWebOptimizationExample/Content/js/second.js
@@ -0,0 +1,1 @@
+﻿console.log('second script is active');
